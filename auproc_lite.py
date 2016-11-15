7cffdc838932b89250615286af8a725be0667e90
Ngoc Tran <ngoc@underlandian.com>
2016-11-15 02:10:36 -0500

    improved build, sync w iPhone app

--- auproc_lite.py
+++ auproc_lite.py
@@ -13,9 +13,11 @@ import thread
 from colorsys import hsv_to_rgb
 import sys
 import socket
+from random import random
 from ledctrl import *
 
 # preset management part
+valentine = False
 
 # copied somewhere online, thank you very much and sorry I could not credit you
 def incoming(host, port):
@@ -31,36 +33,58 @@ def incoming(host, port):
   return request.makefile('r', 0)
 
 def portListener(host, port):
+  global valentine
   while True:
     for line in incoming(host, port):
       if len(line)>6:
         line = line[0:6]
       print 'data incoming to port', port,': ', line
       if len(line)==6:
-      	try:
-          red = int(line[0:2],16)
-          green = int(line[2:4],16)
-          blue = int(line[4:6],16)
-          print 'Custom light color detected.'
-          setColor(red, green, blue)
-          if (red+green+blue == 0):
-            print 'Light turned off.'
-        except:
-          pass
+        if line == 'alloff':
+          valentine = False
+        elif line == 'moodie':
+          valentine = True
+        else:
+          try:
+            red = int(line[0:2],16)
+            green = int(line[2:4],16)
+            blue = int(line[4:6],16)
+            print 'Custom light color detected.'
+            setColor(red, green, blue)
+            if (red+green+blue == 0):
+              print 'Light turned off.'
+            valentine = False
+          except:
+            pass
 
-# cycle through hue, the higher the peak the more changed
-def hueIncrement(hue,x):
-  hue += x**4
-  if (hue>1):
-    hue-=1
-  return hue
-
-hue = 0.
+minh = 190.
+maxh = 320.
+up_d = 1.
+itrv = 0.01
+loop = 10.
+diff = maxh - minh
+last = time()
+hue = minh
 
 thread.start_new_thread(portListener, ('', 2112))
-r,g,b = hsv_to_rgb(hue, 1, 1)
-setColor(r,g,b)
 
 while True:
-  pass
-
+  if valentine:
+    now = time()
+    if now-last < itrv:
+      continue
+    if now-last < 2 * itrv:
+      hue += up_d * diff / loop * itrv
+    last = now
+    if hue > maxh:
+      up_d = -1.
+      hue = 2. * maxh - hue
+    if hue < minh:
+      up_d = 1.
+      hue = 2 * minh - hue
+    r,g,b = hsv_to_rgb(hue/360., 0.5, 1)
+    r *= 255
+    g *= 255
+    b *= 255
+    print r,g,b
+    setColor(r,g,b)
